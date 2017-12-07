28e8199da3666ad4f790ad509593d75c50983a7f
David Leonard <d+git@adaptive-enterprises.com>
2017-12-07 23:57:30 +1000

    add list macros

---
+++ list.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/* A doubly-linked list element is any struct with
+ * a 'next' and 'prevp' field, where every
+ * linked element maintains this invariant:
+ *          (*p->prevp == p)
+ */
+
+/* Insert unlinked p at a head pointer. After insertion, head = p. */
+#define INSERT(p, head) do { \
+	(p)->prevp = &(head); \
+	(p)->next = (head); \
+	if (head) (head)->prevp = &(p)->next; \
+	(head) = (p); \
+    } while (0)
+
+/* Remove a linked p, leaving it unlinked. */
+#define REMOVE(p) do { \
+	if ((p)->next) (p)->next->prevp = (p)->prevp; \
+	*(p)->prevp = (p)->next; \
+    } while (0)
+
+#define LINK(T) T *next, **prevp
+
+#define NEXT(p) (p)->next
